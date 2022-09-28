900324271e0fe1ca8452c8a3966e5ede8b18e6f1
RewaMal <114403485+RewaMal@users.noreply.github.com>
2022-09-28 09:49:02 +0100

    Update primes.py

--- primes.py
+++ primes.py
@@ -3,4 +3,13 @@
 
 def primes(number_of_primes):
     list = []
+    if number_of_primes == 0:
+        raise TypeError(f'Enterd value: {number_of_primes} is a zero, work with numbers greateer then zero only.')
+    elif number_of_primes <0:
+        raise TypeError(f'Entered value: {number_of_primes} is negative, work with positive numeber only.')
+    else: 
+        prime_num =[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+        
+        for x in range(0,number_of_primes):
+            list.append(prime_num[x])
     return list
