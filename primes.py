a41e5beae32d18a4b52f7712460590ede684be9f
RewaMal <114403485+RewaMal@users.noreply.github.com>
2022-09-28 09:52:29 +0100

    Updates primes.py

--- primes.py
+++ primes.py
@@ -6,7 +6,7 @@ def primes(number_of_primes):
     if number_of_primes == 0:
         raise TypeError(ValueError)
     elif number_of_primes <0:
-        raise TypeError(f'Entered value: {number_of_primes} is negative, work with positive numeber only.')
+        raise TypeError(ValueError)
     else: 
         prime_num =[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
         
