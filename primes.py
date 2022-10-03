7b3a9fc66e898d9a2fa6ea8de3a6e18331a2595b
RewaMal <114403485+RewaMal@users.noreply.github.com>
2022-10-03 11:02:07 +0100

    Update primes.py

--- primes.py
+++ primes.py
@@ -4,7 +4,7 @@
 def primes(number_of_primes):
     list = []
     if number_of_primes <= 0:
-        raise valueError("ValueErrorttt")
+        raise ValueError("valueError")
     else: 
         prime_num =[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
         
